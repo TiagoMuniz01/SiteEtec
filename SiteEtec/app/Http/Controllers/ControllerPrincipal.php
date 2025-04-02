a838991108ef73f28f565502325620fe44871bf7
Tiago Pimentel Muniz <131329736+TiagoMuniz01@users.noreply.github.com>
2025-04-02 10:03:34 -0300

    .

--- SiteEtec/app/Http/Controllers/ControllerPrincipal.php
+++ SiteEtec/app/Http/Controllers/ControllerPrincipal.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class ControllerPrincipal extends Controller
 {
-    //
+    public function home(){
+        return view('site.Home');
+    }
+/*
+    Public function rota1(){
+        return view('site.rota1');
+    }
+
+    Public function rota2(){
+        return view('site.rota2');
+    }
+    
+    Public function rota3(){
+        return view('site.rota3');
+    }
+
+    Public function rota4(){
+        return view('site.rota4');
+    }*/
 }
--- SiteEtec/routes/web.php
+++ SiteEtec/routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\ControllerPrincipal;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [ControllerPrincipal::class, 'home'])->name('');
+Route::get('/', [ControllerPrincipal::class, 'Home'])->name('');
 /*Route::get('/', [ControllerPrincipal::class, ''])->name(''); 
 Route::get('/', [ControllerPrincipal::class, ''])->name(''); 
 Route::get('/', [ControllerPrincipal::class, ''])->name(''); 
